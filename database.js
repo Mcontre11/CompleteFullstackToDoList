add1893f97ecb9e346e1ee5a2cb06074ae27fe33
Monica Contreras <78456343+Mcontre11@users.noreply.github.com>
2021-10-25 14:03:33 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : '', 
-    'dbName': ''
+    'url' : 'mongodb+srv://Mcontreras11:dTxbnYBe2XZjRcGJ@cluster0.2exur.mongodb.net/cluster0?retryWrites=true&w=majority', 
+    'dbName': 'todo', 
+  
 };
